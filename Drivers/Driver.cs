7fb51cfa571ac3058ef859792dabc36759cfef4b
Dump <thewaytoqa@gmail.com>
2023-03-29 22:38:14 -0400

    removed wdriver

--- Drivers/Driver.cs
+++ Drivers/Driver.cs
@@ -4,6 +4,14 @@ using System;
 
 namespace csharp.qa.Drivers{
     public abstract class Driver {
-        
+        public abstract string Name { get; }
+        public abstract string Description { get; }
+        public abstract string Version { get; }
+
+    }
+    public abstract void lol()
+    {
+
+        Console.WriteLine("lol");
     }
 }
